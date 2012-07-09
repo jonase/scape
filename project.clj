05f07235104bd082af1c50077ac5c8aad9252b8f
Jonas Enlund <jonas.enlund@gmail.com>
2012-07-09 11:54:42 +0300

    Update clojurescript version to 0.0-1443

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1424"]
+                 [org.clojure/clojurescript "0.0-1443"]
                  [domina "1.0.0-SNAPSHOT"]
                  [com.datomic/datomic "0.1.3164"]])
