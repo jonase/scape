58a41414abec0cdfa2f383734383ff7423645003
Jonas Enlund <jonas.enlund@gmail.com>
2012-07-17 19:10:34 +0300

    Refactor schema definition

--- src/scape/schema.clj
+++ src/scape/schema.clj
@@ -1,181 +1,44 @@
-(ns scape.schema)
+(ns scape.schema
+  (:use [datomic.api :only [tempid]]))
+
+(defn attribute
+  ([ident type cardinality]
+     (attribute ident type cardinality nil))
+  ([ident type cardinality doc]
+     (merge
+      {:db/id (tempid :db.part/db)
+       :db/ident ident
+       :db/valueType (keyword "db.type" (name type))
+       :db/cardinality (keyword "db.cardinality" (name cardinality))
+       :db.install/_attribute :db.part/db}
+      (when doc
+        {:db/doc doc}))))
 
 (def schema
-  [;; Common
-   {:db/id #db/id[:db.part/db -1]
-    :db/ident :ast/op
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc ":op"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/top-level
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "Top level form?"
-    :db.install/_attribute :db.part/db}
+  (map #(apply attribute %)
+       [[:ast/op             :keyword :one]
+        [:ast/top-level      :boolean :one]
+        [:ast/statement      :ref     :many]
+        [:ast/ret            :ref     :one]
+        [:ast/child          :ref     :many]
+        [:ast/form           :string  :one]
+        [:ast/line           :long    :one]
+        [:ast/name           :keyword :one]
+        [:ast/ns             :keyword :one]
+        [:ast/arg            :ref     :many]
+        [:ast.constant/type  :string  :one]
+        [:ast.if/test        :ref     :one]
+        [:ast.if/then        :ref     :one]
+        [:ast.if/else        :ref     :one]
+        [:ast/init           :ref     :one]
+        [:ast.var/local      :boolean :one]
+        [:ast.fn/method      :ref     :many]
+        [:ast.fn/variadic    :boolean :one]
+        [:ast.fn/fixed-arity :long    :one]
+        [:ast.throw/expr     :ref     :one]
+        [:ast.let/loop       :boolean :one]
+        [:ast.let/binding    :ref     :many]
+        [:ast.invoke/f       :ref     :one]]))
    
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/statement
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "A block statement"
-    :db.install/_attribute :db.part/db}
 
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/ret
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "A block return expression"
-    :db.install/_attribute :db.part/db}
    
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/child
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "A child of an ast node"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/form
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "String representation of the form"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/line
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/doc "Line number"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/file
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "file name"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/name
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "name"
-    :db.install/_attribute :db.part/db}
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/ns
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "namespace"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/arg
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Arguments"
-    :db.install/_attribute :db.part/db}
-      
-   ;; :constant
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.constant/type
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Type string of constant"
-    :db.install/_attribute :db.part/db}
-
-   ;; :if
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.if/test
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Test part of an if expression"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.if/then
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Then part of an if expression"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.if/else
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Else part of an if expression"
-    :db.install/_attribute :db.part/db}
-
-   ;; :def
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast/init
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "init"
-    :db.install/_attribute :db.part/db}
-
-   ;; :var
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.var/local
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "is the var local or not?"
-    :db.install/_attribute :db.part/db}
-
-   ;; :fn
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.fn/method
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "A function method"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.fn/variadic
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/many
-    :db/doc "A function method"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.fn/fixed-arity
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/many
-    :db/doc "A function method"
-    :db.install/_attribute :db.part/db}
-
-   ;; Throw
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.throw/expr
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Throw expression"
-    :db.install/_attribute :db.part/db}
-
-   ;; Let
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.let/loop
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "Is this let expr a loop?"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.let/binding
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Let or loop binding"
-    :db.install/_attribute :db.part/db}
-
-   ;; :invoke
-   {:db/id #db/id[:db.part/db]
-    :db/ident :ast.invoke/f
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Function being invoked"
-    :db.install/_attribute :db.part/db}
-
-   ])
