0db62c2b60d400a9452d5765ae198926ffbdc881
Jonas Enlund <jonas.enlund@gmail.com>
2012-08-24 06:18:45 +0300

    Update to latest clojure and datomic releases

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :url "https://github.com/jonase/scape"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-alpha4"]
                  [org.clojure/clojurescript "0.0-1450"]
                  [domina "1.0.0-SNAPSHOT"]
-                 [com.datomic/datomic-free "0.8.3423"]])
+                 [com.datomic/datomic-free "0.8.3438"]])
 
