887301943f1def9df82ec6d575baee93b2d094ad
Jonas Enlund <jonas.enlund@gmail.com>
2012-10-29 13:12:32 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :url "https://github.com/jonase/scape"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-alpha4"]
-                 [org.clojure/clojurescript "0.0-1450"]
-                 [domina "1.0.0-SNAPSHOT"]
-                 [com.datomic/datomic-free "0.8.3470"]])
+  :dependencies [[org.clojure/clojure "1.5.0-beta1"]
+                 [org.clojure/clojurescript "0.0-1513"]
+                 [domina "1.0.1"]
+                 [com.datomic/datomic-free "0.8.3563"]])
 
